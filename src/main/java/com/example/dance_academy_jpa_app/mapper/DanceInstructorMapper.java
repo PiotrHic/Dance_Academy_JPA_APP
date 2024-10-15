122e637d40ca11674a932b18892c338c5156a458
PiotrHic <piotrhic@gmail.com>
2024-10-15 13:09:42 +0200

    Create Dance Instructor Mapper. Closes #16.

---
+++ src/main/java/com/example/dance_academy_jpa_app/mapper/DanceInstructorMapper.java
@@ -0,0 +1,26 @@
+package com.example.dance_academy_jpa_app.mapper;
+
+import com.example.dance_academy_jpa_app.domain.DanceInstructor;
+import com.example.dance_academy_jpa_app.domain.Dancer;
+import com.example.dance_academy_jpa_app.dto.DanceInstructorDTO;
+import com.example.dance_academy_jpa_app.dto.DancerDTO;
+
+public class DanceInstructorMapper {
+
+    public DanceInstructorDTO danceInstructorToDanceInstructorDTO(DanceInstructor danceInstructor){
+
+        return DanceInstructorDTO.builder()
+                .id(danceInstructor.getId())
+                .name(danceInstructor.getName())
+                .yearsOfExperience(danceInstructor.getYearsOfExperience())
+                .build();
+    }
+
+    public DanceInstructor danceInstructorDTOToDanceInstructor(DanceInstructorDTO danceInstructorDTO){
+        return DanceInstructor.builder()
+                .id(danceInstructorDTO.getId())
+                .name(danceInstructorDTO.getName())
+                .yearsOfExperience(danceInstructorDTO.getYearsOfExperience())
+                .build();
+    }
+}
