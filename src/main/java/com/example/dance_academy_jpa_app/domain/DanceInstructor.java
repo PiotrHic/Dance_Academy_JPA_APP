43dfb91b1261104ad20ddd7bd7e12cac18be0ef3
PiotrHic <piotrhic@gmail.com>
2024-10-15 12:56:55 +0200

    Create Dance Instructor Class. Closes #13.

---
+++ src/main/java/com/example/dance_academy_jpa_app/domain/DanceInstructor.java
@@ -0,0 +1,29 @@
+package com.example.dance_academy_jpa_app.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "Dance_Instructor_Table")
+@Entity
+public class DanceInstructor extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "dance_instructor_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "dance_instructor_name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "Years_Of_Experience")
+    private int yearsOfExperience;
+}
