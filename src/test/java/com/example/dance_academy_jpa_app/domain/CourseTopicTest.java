535ef27bc712fdc97b2306a5b49b064799e3e60f
PiotrHic <piotrhic@gmail.com>
2024-10-15 14:21:32 +0200

    Create TDD for CourseTopic. Closes #21.

---
+++ src/test/java/com/example/dance_academy_jpa_app/domain/CourseTopicTest.java
@@ -0,0 +1,50 @@
+package com.example.dance_academy_jpa_app.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CourseTopicTest {
+
+    DanceTopic danceTopic = DanceTopic.builder()
+            .id(1)
+            .name("test").build();
+    @Test
+    public void setId(){
+        //given
+        Integer expected = 10;
+        //when
+        danceTopic.setId(expected);
+        //then
+        Assertions.assertEquals(expected, danceTopic.getId());
+    }
+
+    @Test
+    public void getId(){
+        //given
+        Integer expected = 10;
+        //when
+        danceTopic.setId(expected);
+        //then
+        Assertions.assertEquals(expected, danceTopic.getId());
+    }
+
+    @Test
+    public void setName(){
+        //given
+        String expected = "name";
+        //when
+        danceTopic.setName(expected);
+        //then
+        Assertions.assertEquals(expected, danceTopic.getName());
+    }
+
+    @Test
+    public void getName(){
+        //given
+        String expected = "name";
+        //when
+        danceTopic.setName(expected);
+        //then
+        Assertions.assertEquals(expected, danceTopic.getName());
+    }
+}
