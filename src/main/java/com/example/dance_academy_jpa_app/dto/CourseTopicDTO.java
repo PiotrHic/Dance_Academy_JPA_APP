e9d4840218a458d06f4f30836853fb6d8f757077
PiotrHic <piotrhic@gmail.com>
2024-10-15 14:28:34 +0200

    Create CourseTopic DTO. Closes #23.

---
+++ src/main/java/com/example/dance_academy_jpa_app/dto/CourseTopicDTO.java
@@ -0,0 +1,14 @@
+package com.example.dance_academy_jpa_app.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CourseTopicDTO {
+
+    private Integer id;
+    private String name;
+}
