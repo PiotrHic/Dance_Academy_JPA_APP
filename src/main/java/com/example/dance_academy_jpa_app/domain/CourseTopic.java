eb69fbcc3282c7d263a4a9b8039ed44cc39c1473
PiotrHic <piotrhic@gmail.com>
2024-10-15 14:25:29 +0200

    Create CourseTopic Class. Closes #22.

---
+++ src/main/java/com/example/dance_academy_jpa_app/domain/CourseTopic.java
@@ -0,0 +1,27 @@
+package com.example.dance_academy_jpa_app.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "Course_Topic_Table")
+@Entity
+public class CourseTopic extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "course_topic_id",nullable = false)
+    private Integer id;
+
+    @Column(name = "course_topic_id", nullable = false, unique = true)
+    private String name;
+
+}
