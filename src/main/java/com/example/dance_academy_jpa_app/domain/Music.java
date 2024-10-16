03cc6f0f4bac37c68da428054f61269f735a7dad
PiotrHic <piotrhic@gmail.com>
2024-10-16 09:52:07 +0200

    Create Music Class. Closes #29.

---
+++ src/main/java/com/example/dance_academy_jpa_app/domain/Music.java
@@ -0,0 +1,15 @@
+package com.example.dance_academy_jpa_app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Music{
+
+    private String nameOfTheSong;
+}
