5e25d700187787314d7f1ef2e35a2cbe64c578e6
PiotrHic <piotrhic@gmail.com>
2024-10-16 11:33:28 +0200

    Create DanceMove Class. Closes #32.

---
+++ src/main/java/com/example/dance_academy_jpa_app/domain/DanceMove.java
@@ -0,0 +1,16 @@
+package com.example.dance_academy_jpa_app.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DanceMove {
+
+    private String typeOfTheMove;
+}
