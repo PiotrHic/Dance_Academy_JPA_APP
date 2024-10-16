f465e6ee96ebbfc0543a9cd989184d550d8e7e6e
PiotrHic <piotrhic@gmail.com>
2024-10-16 09:47:22 +0200

    Create TDD for Music Class. Closes #28.

---
+++ src/test/java/com/example/dance_academy_jpa_app/domain/MusicTest.java
@@ -0,0 +1,31 @@
+package com.example.dance_academy_jpa_app.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MusicTest {
+
+    Music music = Music.builder()
+                .nameOfTheSong("Happy")
+                .build();
+
+    @Test
+    public void setNameOfTheSong(){
+        // given
+        String expected = "Sappy";
+        // when
+        music.setNameOfTheSong(expected);
+        // then
+        Assertions.assertEquals(expected, music.getNameOfTheSong);
+    }
+
+    @Test
+    public void getNameOfTheSong(){
+        // given
+        String expected = "Qando";
+        // when
+        music.setNameOfTheSong(expected);
+        // then
+        Assertions.assertEquals(expected, music.getNameOfTheSong);
+    }
+}
