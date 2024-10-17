878c2c2ab84198e0eb05b8e75e1a3500d997b648
PiotrHic <piotrhic@gmail.com>
2024-10-17 09:19:51 +0200

    Create TDD for DanceCourse Repository. Closes #37.

---
+++ src/test/java/com/example/dance_academy_jpa_app/repository/DanceCourseRepositoryTest.java
@@ -0,0 +1,120 @@
+package com.example.dance_academy_jpa_app.repository;
+
+import com.example.dance_academy_jpa_app.domain.DanceCourse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class DanceCourseRepositoryTest {
+
+    @Autowired
+    DanceCourseRepository danceCourseRepository;
+
+    DanceCourse first = DanceCourse.builder()
+            .name("test1")
+            .build();
+
+    DanceCourse second = DanceCourse.builder()
+            .name("test2")
+            .build();
+
+    @BeforeEach
+    void setUp(){
+        danceCourseRepository.deleteAll();
+    }
+
+
+    @Test
+    void createDanceCourse(){
+        long count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(0);
+
+        danceCourseRepository.save(first);
+
+        count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    void getAllDanceCourses(){
+        long count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(0);
+
+        danceCourseRepository.save(first);
+        danceCourseRepository.save(second);
+
+        count = danceCourseRepository.findAll().size();
+        assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    void getOneDanceCourseById(){
+
+        long count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(0);
+
+        DanceCourse saved = danceCourseRepository.save(first);
+
+        count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(1);
+
+        DanceCourse founded = danceCourseRepository.getReferenceById(first.getId());
+
+        assertThat(founded.getName()).isEqualTo("test1");
+
+
+    }
+
+    @Test
+    void deleteDanceCourseById(){
+
+        long count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(0);
+
+        danceCourseRepository.save(first);
+
+        count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(1);
+
+        danceCourseRepository.deleteById(first.getId());
+
+        count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(0);
+    }
+
+
+    @Test
+    void deleteAllDanceCoursesId() {
+        long count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(0);
+
+        danceCourseRepository.save(first);
+        danceCourseRepository.save(second);
+
+        count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(2);
+
+        danceCourseRepository.deleteAll();
+
+        count = danceCourseRepository.count();
+
+        assertThat(count).isEqualTo(0);
+
+    }
+}
