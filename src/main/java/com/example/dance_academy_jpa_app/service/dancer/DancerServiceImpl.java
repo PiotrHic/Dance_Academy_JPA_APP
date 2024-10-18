6d99c7884f14a050056f6d37dc79a9a3a702c793
PiotrHic <piotrhic@gmail.com>
2024-10-18 15:49:29 +0200

    Create Dancer Service Implementation. Closes #52.

---
+++ src/main/java/com/example/dance_academy_jpa_app/service/dancer/DancerServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.dance_academy_jpa_app.service.dancer;
+
+import com.example.dance_academy_jpa_app.domain.Dancer;
+import com.example.dance_academy_jpa_app.repositories.DancerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class DancerServiceImpl implements DancerService {
+
+    private final DancerRepository dancerRepository;
+    @Override
+    public Dancer createDancer(Dancer dancer) {
+        return dancerRepository.save(dancer);
+    }
+
+    @Override
+    public Dancer getDancer(Integer id) {
+        if(!dancerRepository.existsById(id)){
+            throw new JPAEntityNotFoundException("Dancer with id: " + id + " was not found!");
+        }
+        return dancerRepository.getById(id);
+    }
+
+
+    @Override
+    public List<Dancer> getAllDancers(){
+        return dancerRepository.findAll();
+    }
+    @Override
+    public Dancer updateDancer(Integer id, Dancer dancer) {
+        if(!dancerRepository.existsById(id)){
+            throw new JPAEntityNotFoundException("Dancer with id: " + id + " was not found!");
+        }
+        Dancer toUpdate = dancerRepository.getReferenceById(id);
+        toUpdate.setName(dancer.getName());
+        toUpdate.setName(dancer.getName());
+        toUpdate.setDanceCourses(dancer.getDanceCourses());
+        toUpdate.setCreatedAt(dancer.getCreatedAt());
+        toUpdate.setCreatedBy(dancer.getCreatedBy());
+        toUpdate.setLastModifiedAt(dancer.getLastModifiedAt());
+        toUpdate.setLastModifiedBy(dancer.getLastModifiedBy());
+        return dancerRepository.save(toUpdate);
+    }
+
+    @Override
+    public String deleteDancer(Integer id) {
+        if(!dancerRepository.existsById(id)){
+            throw new JPAEntityNotFoundException("Dancer with id: " + id + " was not found!");
+        }
+        Dancer deleted = dancerRepository.getById(id);
+        String result = "Dancer with id: " + deleted.getId() + " and name: " + deleted.getName() + " was deleted!";
+        dancerRepository.deleteById(id);
+        return result;
+    }
+
+    @Override
+    public void deleteAllDancers() {
+        dancerRepository.deleteAll();
+    }
+}
