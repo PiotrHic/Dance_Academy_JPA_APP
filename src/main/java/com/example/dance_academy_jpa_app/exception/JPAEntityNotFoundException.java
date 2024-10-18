7491a3aa7e28f72ec4e1973a737793e616ea9c00
PiotrHic <piotrhic@gmail.com>
2024-10-18 15:51:39 +0200

    Create JPAEntityNotFoundException. Closes #53.

---
+++ src/main/java/com/example/dance_academy_jpa_app/exception/JPAEntityNotFoundException.java
@@ -0,0 +1,15 @@
+package com.example.dance_academy_jpa_app.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such Entity in Repository")
+public class JPAEntityNotFoundException extends EntityNotFoundException {
+
+    private String message;
+    public JPAEntityNotFoundException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
