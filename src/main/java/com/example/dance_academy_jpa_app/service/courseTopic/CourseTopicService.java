7bea4f128ec371e6c684d25a4fcbc9a0330cc676
PiotrHic <piotrhic@gmail.com>
2024-10-18 18:37:54 +0200

    Create Course Topic Service. Closes #63.

---
+++ src/main/java/com/example/dance_academy_jpa_app/service/courseTopic/CourseTopicService.java
@@ -0,0 +1,20 @@
+package com.example.dance_academy_jpa_app.service.courseTopic;
+
+import com.example.dance_academy_jpa_app.domain.CourseTopic;
+
+import java.util.List;
+
+public interface CourseTopicService {
+
+    CourseTopic createTopic(CourseTopic courseTopic);
+
+    CourseTopic getCourseTopic(Integer id);
+
+    List<CourseTopic> getCourseTopics();
+
+    CourseTopic updateCourseTopic(Integer id, CourseTopic courseTopic);
+
+    String deleteCourseTopicByID(Integer id);
+
+    void deleteAllDancers();
+}
