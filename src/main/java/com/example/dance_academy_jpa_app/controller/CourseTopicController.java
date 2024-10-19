d84cef528f7e10d3ac988e8387937b5f8d02bd93
PiotrHic <piotrhic@gmail.com>
2024-10-19 15:02:51 +0200

    Create Open API for Course Topic. Closes #77.

--- src/main/java/com/example/dance_academy_jpa_app/controller/CourseTopicController.java
+++ src/main/java/com/example/dance_academy_jpa_app/controller/CourseTopicController.java
@@ -7,6 +7,10 @@ import com.example.dance_academy_jpa_app.dto.CourseTopicDTO;
 import com.example.dance_academy_jpa_app.dto.DancerDTO;
 import com.example.dance_academy_jpa_app.mapper.CourseTopicMapper;
 import com.example.dance_academy_jpa_app.service.courseTopic.CourseTopicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +30,10 @@ public class CourseTopicController {
     private final String API_PATH_ID = "/{courseTopicID}";
     private final String ID_PATH = "courseTopicID";
 
+    @Operation(summary = "It adds a new Course Topic to the database")
+    @ApiResponse(responseCode = "201",
+            description = "Add new Course Topic to the database",
+            content = {@Content(mediaType =  "application/json")})
     @PostMapping
     ResponseEntity<CourseTopicDTO> createCourseTopic(@RequestBody CourseTopicDTO courseTopicDTO){
         CourseTopic toSave = courseTopicService
@@ -34,6 +42,15 @@ public class CourseTopicController {
         return new ResponseEntity<>(courseTopicMapper.courseTopicToCourseTopicDTO(toSave),
                 HttpStatus.valueOf(201));
     }
+    @Operation(summary = "It updates Course Topic with the new data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Update Course Topic to the database",
+                    content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Course Topic was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @PutMapping(API_PATH_ID)
     ResponseEntity <CourseTopicDTO> updateCourseTopicById(@PathVariable(ID_PATH) Integer courseTopicID,
                                                      @RequestBody CourseTopicDTO courseTopicDTO) {
@@ -42,12 +59,24 @@ public class CourseTopicController {
         return new ResponseEntity<>(courseTopicMapper.courseTopicToCourseTopicDTO(updated), HttpStatus.OK);
     }
 
+    @Operation(summary = "It brings one Course Topic from the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Get one Course Topic from the database",
+                    content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Course Topic was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @GetMapping(API_PATH_ID)
     ResponseEntity <CourseTopicDTO> getCourseTopicById(@PathVariable(ID_PATH) Integer courseTopicID) {
         CourseTopic founded = courseTopicService.getCourseTopic(courseTopicID);
         return new ResponseEntity<>(courseTopicMapper.courseTopicToCourseTopicDTO(founded), HttpStatus.OK);
     }
-
+    @Operation(summary = "Takes all Course Topics from the database")
+    @ApiResponse(responseCode = "200",
+            description = "Gives all Course Topics from the database",
+            content = {@Content(mediaType =  "application/json")})
     @GetMapping
     ResponseEntity <List<CourseTopicDTO>> getCourseTopics(){
         List<CourseTopicDTO> courseTopicDTOs = courseTopicService
@@ -58,12 +87,25 @@ public class CourseTopicController {
         return new ResponseEntity<>(courseTopicDTOs, HttpStatus.OK);
     }
 
+    @Operation(summary = "It deletes one Course Topics from the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Deletes one Course Topics from the database",
+                    content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Course Topics was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @DeleteMapping(API_PATH_ID)
     ResponseEntity <String> deleteCourseTopicById(@PathVariable(ID_PATH) Integer courseTopicID){
         String deleted = courseTopicService.deleteCourseTopicByID(courseTopicID);
         return new ResponseEntity<>(deleted, HttpStatus.OK);
     }
 
+    @Operation(summary = "Deletes all Course Topics from the database")
+    @ApiResponse(responseCode = "200",
+            description = "Deletes all Course Topics from the database",
+            content = {@Content(mediaType =  "application/json")})
     @DeleteMapping()
     ResponseEntity <String> deleteAllDancers(){
         courseTopicService.deleteAllCourseTopics();
